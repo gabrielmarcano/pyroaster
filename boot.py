61c2bfbd7a3c27d86826ad44f90756a655809c9a
Gabriel Marcano <gabrielmarcano120@gmail.com>
2024-11-28 18:44:56 -0400

    format

--- boot.py
+++ boot.py
@@ -1,7 +1,8 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
+# import esp
+# esp.osdebug(None)
 import webrepl
+
 webrepl.start()
 
 
@@ -11,14 +12,16 @@ def do_connect():
     """
     import config
     import network
+
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
+        print("connecting to network...")
         wlan.connect(config.WIFI_SSID, config.WIFI_PASSWD)
         while not wlan.isconnected():
             pass
-    print('network config:', wlan.ipconfig('addr4'))
+    print("network config:", wlan.ipconfig("addr4"))
+
 
 # Connect to WLAN
 do_connect()
