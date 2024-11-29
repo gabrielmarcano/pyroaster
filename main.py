c3c6498b046010c955e2bbb98f7f76690a80a3aa
Gabriel Marcano <gabrielmarcano120@gmail.com>
2024-11-29 13:27:27 -0400

    new project structure

--- main.py
+++ main.py
@@ -1,86 +1,142 @@
-from machine import Pin, SoftI2C # type: ignore
+import machine
 from machine_i2c_lcd import I2cLcd
 from max6675 import MAX6675
-from dht import DHT22 # type: ignore
-from time import sleep
+from dht import DHT22
+import network
+import _thread
+import time
+import random
+import utils
 
-# Define MAX6675 pins
-MAX_SCK = Pin(5, Pin.OUT)
-MAX_CS = Pin(23, Pin.OUT)
-MAX_SO = Pin(19, Pin.IN)
+from http_server import HttpServer
+from logger import SimpleLogger
 
-# Define DHT22 pin
-DHT_PIN = Pin(18)
+logger = SimpleLogger()
 
-# Define the LCD I2C address, dimensions and pins
+MAX_SCK = machine.Pin(5, machine.Pin.OUT)
+MAX_CS = machine.Pin(23, machine.Pin.OUT)
+MAX_SO = machine.Pin(19, machine.Pin.IN)
+
+DHT_PIN = machine.Pin(18)
+
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
-LCD_SDA = Pin(21)
-LCD_SCL = Pin(22)
+LCD_SDA = machine.Pin(21)
+LCD_SCL = machine.Pin(22)
 
-# Define motor pins
-MOTOR1_PIN = Pin(25) # use internal LED (2) for debugging
-MOTOR2_PIN = Pin(26)
-MOTOR3_PIN = Pin(27)
+MOTOR1_PIN = machine.Pin(25, machine.Pin.OUT)
+MOTOR2_PIN = machine.Pin(26, machine.Pin.OUT)
+MOTOR3_PIN = machine.Pin(27, machine.Pin.OUT)
 
-# Define buzzer pin
-BUZZER_PIN = Pin(14)
+BUZZER_PIN = machine.Pin(14, machine.Pin.OUT)
 
-# Define time selector pins
-TIME_A = Pin(36)
-TIME_B = Pin(34)
-TIME_C = Pin(35)
+TIME_A = machine.Pin(36, machine.Pin.IN)
+TIME_B = machine.Pin(34, machine.Pin.IN)
+TIME_C = machine.Pin(35, machine.Pin.IN)
+TIME_ADDER = machine.Pin(12, machine.Pin.IN)
+TIME_REDUCER = machine.Pin(13, machine.Pin.IN)
 
-# Define time control pins
-TIME_ADDER = Pin(12)
-TIME_REDUCER = Pin(13)
+server = HttpServer()
+server.add_route("/events", server.handle_sse)
+server.add_route("/reset", lambda r: machine.reset(), ["POST"])
 
-# Initialize objects
-max = MAX6675(MAX_SCK, MAX_CS, MAX_SO)
+try:
+    max = MAX6675(MAX_SCK, MAX_CS, MAX_SO)
+except Exception as e:
+    logger.error(f"Failed to initialize the thermocouple with MAX6675: {e}")
+    logger.info(f"Rebooting...")
+    machine.reset()
 
-dht = DHT22(DHT_PIN)
+try:
+    dht = DHT22(DHT_PIN)
+except Exception as e:
+    logger.error(f"Failed to initialize the DHT22: {e}")
+    logger.info(f"Rebooting...")
+    machine.reset()
 
-i2c = SoftI2C(sda=Pin(21), scl=Pin(22), freq=400000)
-lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+try:
+    i2c = machine.SoftI2C(sda=LCD_SDA, scl=LCD_SCL, freq=400000)
+    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-def do_connect():
-    """
-    Connects to a wireless network using the given SSID and password
-    """
-    import config
-    import network # type: ignore
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(config.WIFI_SSID, config.WIFI_PASSWD)
-        while not wlan.isconnected():
-            pass
-    print('network config:', wlan.ipconfig('addr4'))
-
-# Connect to WLAN
-do_connect()
-
-while True:
-    # Themperature
-    print(max.read()) # eg. ??? (% RH)
-
-    # Humidity
-    dht.measure()
-    print(dht.humidity()) # eg. 41.3 (% RH)
-
-    # Clear the LCD
     lcd.clear()
-    # Display two different messages on different lines
-    # By default, it will start at (0,0) if the display is empty
-    lcd.putstr("Hello World!")
-    sleep(2)
-    lcd.clear()
-    # Starting at the second line (0, 1)
+    lcd.putstr("IP:")  # By default, it will start at (0,0) if the display is empty
     lcd.move_to(0, 1)
-    lcd.putstr("Hello World!")
+    lcd.putstr(f"{network.WLAN(network.STA_IF).ipconfig("addr4")[0]}")
+    time.sleep(5)
+except Exception as e:
+    logger.error(f"Failed to initialize LCD: {e}")
+    logger.info(f"Rebooting...")
+    machine.reset()
 
-    sleep(2)
+try:
+    utils.play_melody(BUZZER_PIN)
+except Exception as e:
+    logger.error(f"Failed to play melody: {e}")
 
 
+def read_sensor_data():
+    """
+    Read sensor data and return as a dictionary
+    """
+    try:
+        # dht.measure()
+        # temperature = max.read()
+        # humidity = dht.measure().humidity()
+        temperature = random.randint(10, 40)
+        humidity = random.randint(30, 80)
+        return {"temperature": temperature, "humidity": humidity}
+    except Exception as e:
+        logger.error(f"Failed to read sensor data: {e}")
+        return None
+
+
+def get_time_values():
+    pass
+
+
+def get_motor_states():
+    pass
+
+
+def add_time():
+    pass
+
+
+def reduce_time():
+    pass
+
+
+def refresh_lcd_data():
+    lcd.clear()
+    lcd.putstr("foo")
+    lcd.move_to(0, 1)
+    lcd.putstr("bar")
+
+
+def send_updates_to_server(server: HttpServer):
+    """
+    Send sensor data to the server every second
+    """
+    while True:
+        sensor_data = read_sensor_data()
+        time_values = get_time_values()
+        motor_states = get_motor_states()
+        refresh_lcd_data()
+
+        if sensor_data is not None:
+            server.send_sse(sensor_data, "sensors")
+
+        if time_values is not None:
+            server.send_sse(time_values, "time")
+
+        if motor_states is not None:
+            server.send_sse(motor_states, "states")
+
+        time.sleep(1)
+
+
+# Start the update function in a new thread
+_thread.start_new_thread(send_updates_to_server, (server,))
+
+server.start()
