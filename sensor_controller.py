86739f989348afb947ba8e0dcf4b87748e50b4ef
Gabriel Marcano <gabrielmarcano120@gmail.com>
2025-02-14 18:18:17 -0400

    fix: dht init & return int

--- sensor_controller.py
+++ sensor_controller.py
@@ -1,5 +1,5 @@
 from max6675 import MAX6675
-from dht import DHT22
+import dht
 
 import random
 
@@ -7,7 +7,7 @@ import random
 class SensorController:
     def __init__(self, DHT_PIN, MAX_SCK, MAX_CS, MAX_SO):
         self.__max = MAX6675(MAX_SCK, MAX_CS, MAX_SO)
-        self.__dht = DHT22(DHT_PIN)
+        self.__dht = dht.DHT22(DHT_PIN)
 
         self.__temperature = 0
         self.__humidity = 0
@@ -17,11 +17,9 @@ class SensorController:
         Read sensor data and return as a dictionary
         """
         try:
-            # self.__dht.measure()
-            # self.temperature = self.__max.read()
-            # self.humidity = self.__dht.measure().humidity()
-            self.__temperature = random.randint(100, 180)
-            self.__humidity = random.randint(30, 60)
+            self.__dht.measure()
+            self.__temperature = int(self.__max.read())
+            self.__humidity = int(self.__dht.humidity())
 
             return self.__temperature, self.__humidity
         except Exception as e:
